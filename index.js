edee4afe96f6b1285fb6eb1dc4acd8edb102e6cf
Gavin Eldridge <gchacka@gmail.com>
2022-09-09 19:37:50 -0700

    better way of organizing the addEventListeners

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-    const e = document.querySelectorAll('button');
-    let count = 1;
-    e[0].addEventListener('click', choice(count, 0));
-    e[1].addEventListener('click', choice(count, 1));
-    e[2].addEventListener('click', choice(count, 2));
-    e[3].addEventListener('click', choice(count, 3));
-    e[4].addEventListener('click', choice(count, 4));
-    e[5].addEventListener('click', choice(count, 5));
-    e[6].addEventListener('click', choice(count, 6));
-    e[7].addEventListener('click', choice(count, 7));
-    e[8].addEventListener('click', choice(count, 8));
-    function choice(count, rad){
-        count++;
-        if(count%2==0){
-            e[rad].innerHTML="";
-        }else{
-            de[rad].innerHTML="O";
-        }
-    }
+const btns = document.querySelectorAll("button");
 
-                 
+let count = 1;
+
+console.log(btns);
+
+for(let i = 0; i<btns.length; i++){
+    btns[i].addEventListener("click", () => choice(i));
+}
+
+function choice(rad) {
+  count++;
+  console.log(count);
+  if (count % 2 == 0) {
+    btns[rad].innerHTML = "X";
+  } else {
+    btns[rad].innerHTML = "O";
+  }
+}
